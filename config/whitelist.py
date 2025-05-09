1166d570d2394d1d149f37f7b48f77cdd17eea9f
Erik Kling <kling.erik@icloud.com>
2025-05-09 02:50:19 +0200

    Artikelfilter hinzugefügt. (Zeitraum, Magazine)

---
+++ config/whitelist.py
@@ -0,0 +1,17 @@
+
+WHITELIST = [
+    "ComputerBase",
+    "T3n",
+    "Caschys Blog",
+    "netzwelt",
+    "Giga",
+    "heise online",
+    "Der-postillon.com",
+    "CHIP Online Deutschland",
+    "mobiFlip.de",
+    "PC Games",
+    "PC Games Hardware",
+    "Golem.de",
+    "Curved.de",
+    "futurezone.at"
+]
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import requests
 from dotenv import load_dotenv
 import os
+from config.whitelist import WHITELIST
+from datetime import datetime, timedelta
+
+today = datetime.now()
+days_since_monday = today.weekday()
+last_monday = today - timedelta(days=days_since_monday)
+
 
 load_dotenv()
 NEWS_API = os.getenv("NEWS_API_KEY")
@@ -14,4 +21,10 @@ params = {
 
 response = requests.get(url, params=params)
 data = response.json()
-print(data["articles"][1]["title"])
+
+filtered_articles = []
+
+for article in data["articles"]:
+    published_at = datetime.strptime(article["publishedAt"], "%Y-%m-%dT%H:%M:%SZ")
+    if article["source"]["name"] in WHITELIST and published_at >= last_monday:
+        filtered_articles.append(article)
